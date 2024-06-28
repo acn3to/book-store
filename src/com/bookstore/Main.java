87c58e6afa33aa04c8473a586ce71ec66e0e9430
acn3to <costanetoow@gmail.com>
2024-06-28 15:29:58 -0300

    feat(model): add Book class extending Product

--- src/com/bookstore/Main.java
+++ src/com/bookstore/Main.java
@@ -1,11 +1,14 @@
 package com.bookstore;
 
+import com.bookstore.model.Book;
 import com.bookstore.model.Product;
 
 public class Main {
     public static void main(String[] args) {
         Product product = new Product("Book", 120.0);
+        Book book = new Book("Dune", 159.90, "Frank Herbert", 535);
 
         System.out.println(product);
+        System.out.println(book);
     }
 }
