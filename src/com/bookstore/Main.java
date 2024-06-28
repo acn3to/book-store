084c804d61ac5486a045d3991869bd0682330b4a
acn3to <costanetoow@gmail.com>
2024-06-28 16:27:58 -0300

    feat(util): add BookPrinter class

--- src/com/bookstore/Main.java
+++ src/com/bookstore/Main.java
@@ -2,19 +2,24 @@ package com.bookstore;
 
 import com.bookstore.model.Book;
 import com.bookstore.service.BookService;
+import com.bookstore.util.BookPrinter;
 
 public class Main {
     public static void main(String[] args) {
         BookService bookService = new BookService();
 
+        BookPrinter bookPrinter = new BookPrinter(bookService);
+
         Book book1 = new Book("Dune", 159.90, "Frank Herbert", 535);
-        Book book2 = new Book("Berserk Deluxe Volume 1 ", 343.50, "Kentato Miura", 535);
+        Book book2 = new Book("Berserk Deluxe Volume 1", 343.50, "Kentato Miura", 535);
+        Book book3 = new Book("The Lord of the Rings Deluxe Edition", 201.90, "J. R. R. Tolkien", 1216);
+        Book book4 = new Book("Harry Potter and the Prisoner of Azkaban", 199.90, "J.K. Rowling", 475);
 
         bookService.addBook(book1);
         bookService.addBook(book2);
+        bookService.addBook(book3);
+        bookService.addBook(book4);
 
-        for (Book book : bookService.getAllBooks()) {
-            System.out.println(book.toString());
-        }
+        bookPrinter.printAllBooks();
     }
 }
--- src/com/bookstore/util/BookPrinter.java
+++ src/com/bookstore/util/BookPrinter.java
@@ -1,4 +1,21 @@
 package com.bookstore.util;
 
+import com.bookstore.model.Book;
+import com.bookstore.service.BookService;
+
+import java.util.List;
+
 public class BookPrinter {
+    private final BookService bookService;
+
+    public BookPrinter(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    public void printAllBooks() {
+        List<Book> books = this.bookService.getAllBooks();
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
 }
