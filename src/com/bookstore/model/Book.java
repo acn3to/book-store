77c92ab5d7c6e8cfc41c43578287d2bdd7f682d1
acn3to <costanetoow@gmail.com>
2024-06-28 15:29:58 -0300

    feat(model): add Book class extending Product

--- src/com/bookstore/model/Book.java
+++ src/com/bookstore/model/Book.java
@@ -1,4 +1,38 @@
 package com.bookstore.model;
 
-public class Book {
+public class Book extends Product {
+    private String author;
+    private int pages;
+
+    public Book(String name, double price, String author, int pages) {
+        super(name, price);
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + getName() + '\'' +
+                ", price=" + getPrice() +
+                ", author='" + getAuthor() + '\'' +
+                ", pages=" + getPages() +
+                '}';
+    }
 }
