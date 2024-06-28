0cbbf160e9e84eef166904cd1b79185e1447f074
acn3to <costanetoow@gmail.com>
2024-06-28 14:58:57 -0300

    feat(model): add Product class

--- src/com/bookstore/Main.java
+++ src/com/bookstore/Main.java
@@ -1,7 +1,11 @@
 package com.bookstore;
 
+import com.bookstore.model.Product;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product product = new Product("Book", 120.0);
+
+        System.out.println(product);
     }
 }
