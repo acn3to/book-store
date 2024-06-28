52a581f34780a046eaf86e8df318c54039a39459
acn3to <costanetoow@gmail.com>
2024-06-28 14:58:57 -0300

    feat(model): add Product class

--- src/com/bookstore/model/Product.java
+++ src/com/bookstore/model/Product.java
@@ -1,4 +1,32 @@
 package com.bookstore.model;
 
 public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    protected double getPrice() {
+        return price;
+    }
+
+    protected void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{name='" + getName() + "', price=" + getPrice() + "}";
+    }
 }
